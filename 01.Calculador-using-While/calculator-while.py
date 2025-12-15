0ab60920ed67c120f14001aac3d57ce576072720
Carol Reis <reicaroline@gmail.com>
2025-12-15 18:23:56 -0300

    feat: Add first project and update README.

---
+++ 01.Calculador-using-While/calculator-while.py
@@ -0,0 +1,57 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+def calculator():
+	"""
+	Simple calculator that performs basic operations (+, -, /, *) on two numbers.
+	The program continues to prompt for input until the user chooses to exit.
+	"""
+	while True:							               								
+		number_1 = input('Enter a number: ')         
+		operator = input('Choose an operator (+-/*): ')
+		number_2 = input('Enter another number: ')  
+							
+		valid_numbers = None  
+		num_1_float = 0         
+		num_2_float = 0
+		
+		try:							
+			num_1_float = float(number_1)
+			num_2_float = float(number_2)
+			valid_numbers = True
+		except:
+			valid_numbers = None
+
+		if valid_numbers is None:
+			logger.warning('Oops, one or more numbers are invalid!')
+			continue
+
+		allowed_operators = '+-/*'		
+												
+		if operator not in allowed_operators:	
+			logger.warning('Invalid operator!')
+			continue
+
+		if len(operator) > 1:				
+			logger.warning('Enter only one operator!')
+			continue
+
+		if operator == '+':
+			print(f'{num_1_float} + {num_2_float} =', num_1_float + num_2_float)
+		elif operator == '-':
+			print(f'{num_1_float} - {num_2_float} =', num_1_float - num_2_float)
+		elif operator == '/':
+			print(f'{num_1_float} / {num_2_float} =', num_1_float / num_2_float)
+		elif operator == '*':
+			print(f'{num_1_float} * {num_2_float} =', num_1_float * num_2_float)
+		else:
+			logger.warning('Hm, something is wrong. Try again!')
+			continue
+
+		exit = input('Enter [q] to quit: ').lower().startswith('q')
+		if exit is True:
+			print('Thanks for using the calculator!') 
+			break
+if __name__ == '__main__':
+    calculator()
