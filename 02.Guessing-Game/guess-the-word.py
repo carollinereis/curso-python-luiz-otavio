875ff7be5fe9970398cc9440ca24e0d73b34dc9f
Carol Reis <reicaroline@gmail.com>
2025-12-30 18:36:06 -0300

    feat: add complete hangman game

--- 02.Guessing-Game/guess-the-word.py
+++ 02.Guessing-Game/guess-the-word.py
@@ -1,37 +1,47 @@
+import random
 
 def guessing_word():
-    chosen_word = 'perfum'
+    word_list = ['thought', 'beaver', 'ranch', 'wool', 'cherry', 'pen', 'sibling', 'aunt', 'picture', 'fork']
+    random_words = random.choice(word_list)
     attempts = 0
     right_letters = []
+    missed_letters = []
     
+    print('\n'+ '='*5 + ' Welcome to the Hangman game! ' + '='*5)
+
     while True:
         user_guess = input('\nEnter a letter: ').strip().lower()
 
         if not user_guess or not user_guess.isalpha() or len(user_guess) > 1:
-            print('Please enter letters only!')
+            print('Please enter a single letter!')
             continue
 
         attempts += 1
 
-        if user_guess in chosen_word and user_guess not in right_letters:
-            right_letters.append(user_guess)
-        
-        won = True
+        if user_guess in random_words:
+            if user_guess not in right_letters:
+                right_letters.append(user_guess)
+        else:
+            if user_guess not in missed_letters:
+                missed_letters.append(user_guess)
 
-        for letter in chosen_word:
+        won = True
+        print('Word: ', end=' ')
+        for letter in random_words:
             if letter in right_letters:
                     print(letter, end=' ')
             else: 
                 print('*', end=' ')
                 won = False
-        
+         
         print()
+        print(f'Wrong guesses: {missed_letters}')
 
         if won:
-            print(f'\nCONGRATS, you got it!')
-            print(f'The word was: {chosen_word}')
+            print(f'\nCONGRATS! You got it!')
+            print(f'The word was: {random_words}')
             print(f'Total attempts: {attempts}')
             break
-           
+
 if __name__ == '__main__':
     guessing_word()
